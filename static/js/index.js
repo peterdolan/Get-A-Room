f637b9c6b417a7822fe19873d233e6768196972d
cgirod3 <cgirod3@DN0a2262fa.SUNet>
2016-02-03 16:26:37 -0800

    added js functionality to accommodate bootstrap. Updated the submit button

---
+++ static/js/index.js
@@ -0,0 +1,28 @@
+function addClasses() {
+	var date = document.getElementById("id_date");
+	date.className = "form-control";
+
+	var duration = document.getElementById("id_duration");
+	duration.className = "form-control";
+
+	var time = document.getElementById("id_time");
+	time.className = "form-control";
+
+	var projector = document.getElementById("id_projector");
+	projector.className = "form-control";
+
+	var whiteboard = document.getElementById("id_whiteboard");
+	whiteboard.className = "form-control";
+
+	var windows = document.getElementById("id_windows");
+	windows.className = "form-control";
+
+	var area = document.getElementById("id_area");
+	area.className = "form-control";
+
+	var windows = document.getElementById("id_windows");
+	windows.className = "form-control";
+
+	var flexible = document.getElementById("id_flexible");
+	flexible.className = "form-control";
+}
