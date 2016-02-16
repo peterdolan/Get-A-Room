db9944c2009c84e91fd25f6810d8b018c0bab0d8
jmwebb <jmwebb@stanford.edu>
2016-02-15 22:14:02 -0800

    User now registers/logs in with email. User cannot register with email that already belongs to another user.

--- booker/views.py
+++ booker/views.py
@@ -18,7 +18,7 @@ from .models import *
 from .forms import RoomForm
 from .forms import ReservationForm
 
-@login_required
+# @login_required
 def index(request):
 	if request.method == 'POST':
 		form = RoomForm(request.POST)
@@ -138,119 +138,126 @@ def admin_dashboard(request):
 # taken from:
 # http://www.tangowithdjango.com/book/chapters/login.html
 def register(request):
-    # Like before, get the request's context.
-    context = RequestContext(request)
+	# Like before, get the request's context.
+	context = RequestContext(request)
 
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
-    registered = False
+	# A boolean value for telling the template whether the registration was successful.
+	# Set to False initially. Code changes value to True when registration succeeds.
+	registered = False
 
-    # If it's a HTTP POST, we're interested in processing form data.
-    if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
-        user_form = UserForm(data=request.POST)
-        user_profile_form = UserProfileForm(data=request.POST)
+	# If it's a HTTP POST, we're interested in processing form data.
+	if request.method == 'POST':
+		# Attempt to grab information from the raw form information.
+		# Note that we make use of both UserForm and UserProfileForm.
+		user_form = UserForm(data=request.POST)
+		user_profile_form = UserProfileForm(data=request.POST)
 
-        # If the two forms are valid...
-        if user_form.is_valid() and user_profile_form.is_valid():
-            # Save the user's form data to the database.
-            email = user_form.cleaned_data['email']
-            # username is just the email for that user
-            username = email
-            password = user_form.cleaned_data['password']
-            user = User.objects.create_user(username, email, password)
-            user = user_form.save()
+		# If the two forms are valid...
+		if user_form.is_valid() and user_profile_form.is_valid():
+			# Save the user's form data to the database.
+			email = user_form.cleaned_data['email']
 
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
-            user.set_password(user.password)
-            user.save()
+			if User.objects.get(username=email):
+				return HttpResponse('User with that email already exists!!')
+			# username is just the email for that user
+			# username = email
+			password = user_form.cleaned_data['password']
+			# user = User.objects.create_user(username, email, password)
+			user = user_form.save()
 
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves, we set commit=False.
-            # This delays saving the model until we're ready to avoid integrity problems.
-            profile = user_profile_form.save(commit=False)
-            profile.user = user
+			# Now we hash the password with the set_password method.
+			# Once hashed, we can update the user object.
+			user.set_password(user.password)
+			user.save()
 
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and put it in the UserProfile model.
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
+			user = User.objects.get(pk=user.id)
+			user.username = email
+			user.save()
 
-            # Now we save the UserProfile model instance.
-            profile.save()
+			# Now sort out the UserProfile instance.
+			# Since we need to set the user attribute ourselves, we set commit=False.
+			# This delays saving the model until we're ready to avoid integrity problems.
+			profile = user_profile_form.save(commit=False)
+			profile.user = user
 
-            # Update our variable to tell the template registration was successful.
-            registered = True
+			# Did the user provide a profile picture?
+			# If so, we need to get it from the input form and put it in the UserProfile model.
+			if 'picture' in request.FILES:
+				profile.picture = request.FILES['picture']
 
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
-        else:
-            print user_form.errors, user_profile_form.errors
+			# Now we save the UserProfile model instance.
+			profile.save()
 
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
-    else:
-        user_form = UserForm()
-        user_profile_form = UserProfileForm()
+			# Update our variable to tell the template registration was successful.
+			registered = True
 
-    # Render the template depending on the context.
-    return render_to_response(
-            'booker/register.html',
-            {'user_form': user_form, 'user_profile_form': user_profile_form, 'registered': registered},
-            context)
+		# Invalid form or forms - mistakes or something else?
+		# Print problems to the terminal.
+		# They'll also be shown to the user.
+		else:
+			print user_form.errors, user_profile_form.errors
+
+	# Not a HTTP POST, so we render our form using two ModelForm instances.
+	# These forms will be blank, ready for user input.
+	else:
+		user_form = UserForm()
+		user_profile_form = UserProfileForm()
+
+	# Render the template depending on the context.
+	return render_to_response(
+			'booker/register.html',
+			{'user_form': user_form, 'user_profile_form': user_profile_form, 'registered': registered},
+			context)
 
 
 def user_login(request):
-    # Like before, obtain the context for the user's request.
-    context = RequestContext(request)
+	# Like before, obtain the context for the user's request.
+	context = RequestContext(request)
 
-    # If the request is a HTTP POST, try to pull out the relevant information.
-    if request.method == 'POST':
-        # Gather the username and password provided by the user.
-        # This information is obtained from the login form.
-        username = request.POST['username']
-        password = request.POST['password']
+	# If the request is a HTTP POST, try to pull out the relevant information.
+	if request.method == 'POST':
+		# Gather the username and password provided by the user.
+		# This information is obtained from the login form.
+		username = request.POST['username']
+		password = request.POST['password']
 
-        # Use Django's machinery to attempt to see if the username/password
-        # combination is valid - a User object is returned if it is.
-        user = authenticate(username=username, password=password)
+		# Use Django's machinery to attempt to see if the username/password
+		# combination is valid - a User object is returned if it is.
+		user = authenticate(username=username, password=password)
 
-        # If we have a User object, the details are correct.
-        # If None (Python's way of representing the absence of a value), no user
-        # with matching credentials was found.
-        if user:
-            # Is the account active? It could have been disabled.
-            if user.is_active:
-                # If the account is valid and active, we can log the user in.
-                # We'll send the user back to the homepage.
-                login(request, user)
-                return HttpResponseRedirect('/booker/')
-            else:
-                # An inactive account was used - no logging in!
-                return HttpResponse("Your Rango account is disabled.")
-        else:
-            # Bad login details were provided. So we can't log the user in.
-            print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
+		# If we have a User object, the details are correct.
+		# If None (Python's way of representing the absence of a value), no user
+		# with matching credentials was found.
+		if user:
+			# Is the account active? It could have been disabled.
+			if user.is_active:
+				# If the account is valid and active, we can log the user in.
+				# We'll send the user back to the homepage.
+				login(request, user)
+				return HttpResponseRedirect('/booker/')
+			else:
+				# An inactive account was used - no logging in!
+				return HttpResponse("Your Get-A-Room account is disabled.")
+		else:
+			# Bad login details were provided. So we can't log the user in.
+			print "Invalid login details: {0}, {1}".format(username, password)
+			return HttpResponse("Invalid login details supplied.")
 
-    # The request is not a HTTP POST, so display the login form.
-    # This scenario would most likely be a HTTP GET.
-    else:
-        # No context variables to pass to the template system, hence the
-        # blank dictionary object...
-        return render_to_response('booker/login.html', {}, context)
+	# The request is not a HTTP POST, so display the login form.
+	# This scenario would most likely be a HTTP GET.
+	else:
+		# No context variables to pass to the template system, hence the
+		# blank dictionary object...
+		return render_to_response('booker/login.html', {}, context)
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
 @login_required
 def user_logout(request):
-    # Since we know the user is logged in, we can now just log them out.
-    logout(request)
+	# Since we know the user is logged in, we can now just log them out.
+	logout(request)
 
-    # Take the user back to the homepage.
-    return HttpResponseRedirect('/booker/')
+	# Take the user back to the homepage.
+	return HttpResponseRedirect('/booker/')
 
 @login_required
 def user_profile(request):
