5ab5a9fe7d9c860273596eebef49872aa9cb4790
Alec Powell <alecpowell18@gmail.com>
2016-03-02 15:44:28 -0800

    Final commit before merge to dev branch

--- booker/static/js/calendar.js
+++ booker/static/js/calendar.js
@@ -4,11 +4,6 @@ $(document).ready(function () {
 	var d = date.getDate();
 	var m = date.getMonth();
 	var y = date.getFullYear();
-
-	// var room_info = block.getElementsByTagName("SPAN");
-	// var room_name = room_info[0].innerHTML;
-	// var input = document.getElementById("room-input");
-	// input.value = room_name;
 	
 	var value = document.getElementById("id_building");
 	var building = value.options[value.selectedIndex].value;
@@ -23,29 +18,16 @@ $(document).ready(function () {
 		header: {
 			left: 'prev,next today',
 			center: 'title',
-			right: 'agendaWeek,agendaDay',
-			start: '08:00',
-			end: '02:00'
+			right: 'agendaWeek,agendaDay'
 		},
+		start: '08:00',
+		end: '02:00',
+		allDaySlot: false,
 		height: 1200,
 		editable: true,
 		defaultView: 'agendaWeek',
 		eventColor: '#fed100',
 		eventTextColor: '#222222',
-		// eventSources: [
-	 //        // your event source
-	 //        {
-	 //            events: [ // put the array in the `events` property
-	 //                {
-	 //                    title  : 'Hacking away at new features',
-	 //                    start  : '2016-02-28T12:48:15+00:00',
-	 //                    end	   : '2016-02-28T14:48:15+00:00',
-	 //                    allDay : false
-	 //                }
-	 //            ]
-	 //            // textColor: 'yellow' // an option!
-	 //        }
-	 //    ]
 	 	events: 'eventsfeed/'+building
 	});
 });
